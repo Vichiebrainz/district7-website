4720c4b6c58c3285fd959b6859b9c833a71bc5c6
Kwesi Hervie <herviek2001@gmail.com>
2023-02-25 08:46:02 +0100

    ps refactors

--- src/pages/landlord/post/Form.jsx
+++ src/pages/landlord/post/Form.jsx
@@ -208,7 +208,7 @@ export default function Form() {
         <p className={errorMessageStyles}>{errors.description?.message}</p>
       </label>
 
-      <label>
+      {/* <label>
         <div className="flex justify-between mb-8">
           <div className="relative">
             <div
@@ -225,7 +225,7 @@ export default function Form() {
               }
             />
           </div>
-          {/* <div>
+          <div>
             <label className="relative items-center cursor-pointer">
               <input
                 type="checkbox"
@@ -236,9 +236,9 @@ export default function Form() {
               />
               <div className="w-14 h-7 bg-transparent border-[2px] border-solid border-[#05C002] peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-none after:content-[''] after:absolute after:top-[3px] after:left-[5px]  peer-checked:after:bg-white after:bg-[#05C002] after:border-none after:border after:rounded-full after:h-[22px] after:w-[22px] after:transition-all peer-checked:bg-[#05C002] peer-checked:border-none"></div>
             </label>
-          </div> */}
+          </div>
         </div>
-      </label>
+      </label> */}
 
       {/* <label>
         <span className={labelStyles}>
--- src/pages/landlord/updateProperty/index.jsx
+++ src/pages/landlord/updateProperty/index.jsx
@@ -186,7 +186,7 @@ export default function UpdateProperty() {
         <p className={errorMessageStyles}>{errors.uploaded_images?.message}</p>
       </label> */}
 
-        <label>
+        {/* <label>
           <span className={labelStyles}>Brief Description</span>
           <textarea
             // defaultValue={singleProperty?.description}
@@ -213,7 +213,7 @@ export default function UpdateProperty() {
                 }
               />
             </div>
-            {/* <div>
+            <div>
               <label className="relative items-center cursor-pointer">
                 <input
                   type="checkbox"
@@ -225,9 +225,9 @@ export default function UpdateProperty() {
                 />
                 <div className="w-14 h-7 bg-transparent border-[2px] border-solid border-[#05C002] peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-none after:content-[''] after:absolute after:top-[3px] after:left-[5px]  peer-checked:after:bg-white after:bg-[#05C002] after:border-none after:border after:rounded-full after:h-[22px] after:w-[22px] after:transition-all peer-checked:bg-[#05C002] peer-checked:border-none"></div>
               </label>
-            </div> */}
+            </div>
           </div>
-        </label>
+        </label> */}
 
         <button
           type="submit"
