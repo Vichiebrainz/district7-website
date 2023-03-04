58ce612fb5ead4891643dc1cb5f4ac9e4976bc39
Kwesi Hervie <herviek2001@gmail.com>
2023-03-04 09:46:00 +0100

    Logout accessiblilty

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -2,7 +2,9 @@ import React, { useState, useEffect } from "react";
 import logo from "../assets/logo/logo_solid.png";
 import { MdSettings } from "react-icons/md";
 import { Link, useNavigate } from "react-router-dom";
-import Hamburger from "hamburger-react";
+// import { Squash as Hamburger } from "hamburger-react";
+import { Twirl as Hamburger } from "hamburger-react";
+
 import { userSelector } from "../store/slices/authSlice";
 import { useSelector } from "react-redux";
 
@@ -33,6 +35,20 @@ const MobileNav = ({ avatar }) => {
     };
   }, [popoverRef]);
 
+  const handleProfileClick = () => {
+    navigate(`/${user}/profile`);
+    setOpen(false);
+  };
+  const handleSettingsClick = () => {
+    navigate(`/${user}/settings`);
+    setOpen(false);
+  };
+
+  const handleLogoutClick = () => {
+    navigate(`/${user}/settings?tab=logout`);
+    setOpen(false);
+  };
+
   return (
     <nav className="h-16 w-full z-[999] top-0 bg-white shadow-lg fixed px-4 flex items-center justify-between">
       <div className="flex w-1/3">
@@ -55,22 +71,22 @@ const MobileNav = ({ avatar }) => {
           <Hamburger toggled={isOpen} toggle={setOpen} />
           {isOpen && (
             <div
-              ref={popoverRef}
+              // ref={popoverRef}
               className="bg-white rounded py-2 px-3 text-gray-700 w-fit right-[5%] absolute top-16 md:top-12 md:right-0 shadow-[0px_5px_15px_rgba(0,0,0,0.35)] md:w-[400px]">
               <div className="w-full flex flex-col gap-4 p-4 justify-center divide-x">
                 <div
                   className=" cursor-pointer text-black text-[20px] font-header"
-                  onClick={() => navigate(`/${user}/profile`)}>
+                  onClick={handleProfileClick}>
                   Profile
                 </div>
                 <div
                   className=" cursor-pointer text-black text-[20px] font-header"
-                  onClick={() => navigate(`/${user}/settings`)}>
+                  onClick={handleSettingsClick}>
                   Settings
                 </div>
                 <div
                   className=" cursor-pointer text-black text-[20px] font-header"
-                  onClick={() => navigate(`/${user}/settings?tab=logout`)}>
+                  onClick={handleLogoutClick}>
                   Logout
                 </div>
               </div>
